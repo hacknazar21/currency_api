9d5047a2ab21252e091a9e424ffd63dd54a36eb1
Nazar Cooltaev <nazarcooltaev@192-168-1-103.local>
2024-02-19 17:40:09 +0600

    database config

--- db.js
+++ db.js
@@ -1,10 +1,9 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
-    username: 'postgres',
-    password: 'root',
+    username: 'currency_admin',
+    password: 'a$N"7£>9R9&iO:j836GAA|',
     host: 'localhost',
     database: 'currency_api'
 })
-
 
 module.exports = pool
